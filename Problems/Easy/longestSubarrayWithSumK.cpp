b4748816b158ab064b7b0354510f0339c6e2133d
Piyushkumar99 <73519381+Piyushkumar99@users.noreply.github.com>
2023-11-25 13:23:20 +0000

    added a file

--- Problems/Easy/longestSubarrayWithSumK.cpp
+++ Problems/Easy/longestSubarrayWithSumK.cpp
@@ -1,26 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int longestSubarrayWithSumK(vector<int> a, long long k) {
-  long long sum = a[0];
-  int left = 0, right = 0;
-  int maxLen = 0;
-  int n = a.size();
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+    int left = 0, right = 0; // 2 pointers
+    long long sum = a[0];
+    int maxLen = 0;
+    while (right < n) {
+        // if sum > k, reduce the subarray from left
+        // until sum becomes less or equal to k:
+        while (left <= right && sum > k) {
+            sum -= a[left];
+            left++;
+        }
 
-  while (right < n) {
-    while (left <= right && sum > k) {
-      sum -= a[left];
-      left++;
+        // if sum = k, update the maxLen i.e. answer:
+        if (sum == k) {
+            maxLen = max(maxLen, right - left + 1); //+1 is there because index starts from 0
+        }
+
+        // Move forward thw right pointer:
+        right++;
+        if (right < n) sum += a[right];
     }
-    if (sum == k) {
-      maxLen = max(maxLen, right - left + 1);
-    }
-    right++;
-    if (right < n) {
-      sum += a[right];
-    }
-  }
-  return maxLen;
+
+    return maxLen;
 }
 
 int main() {
@@ -36,7 +40,7 @@ int main() {
     a.push_back(p);
   }
 
-  int sol = longestSubarrayWithSumK(a, k);
+  int sol = getLongestSubarray(a, k);
   cout << sol;
 
   return 0;
